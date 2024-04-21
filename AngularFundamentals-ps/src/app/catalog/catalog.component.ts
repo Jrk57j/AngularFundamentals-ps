b77bade08cab38a70cf796bdcd2b1ff54e83ec81
jrk57j <jrk57j@gmail.com>
2024-04-20 20:33:01 -0400

    turned of google analytics and added looping/binding to catalog

--- AngularFundamentals-ps/src/app/catalog/catalog.component.ts
+++ AngularFundamentals-ps/src/app/catalog/catalog.component.ts
@@ -1,28 +1,77 @@
 import { Component } from '@angular/core';
 import { IProduct } from './product.model';
 
+
 @Component({
   selector: 'bot-catalog',
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css'
 })
+
 export class CatalogComponent {
-  product: IProduct;
+  products: IProduct[];
+  filter: string = '';
 
   constructor() {
-    this.product = {
-      id: 2,
-      description: "A robot who will destory all of mankind-- great for war.",
-      name: "Friendly Bot",
-      imageName: "head-friendly.png",
-      category: "Heads",
-      price: 945.0,
-      discount: 0.2
-    };
+    this.products = [
+      {
+        id: 1,
+        description:
+          "A robot head with an unusually large eye and teloscpic neck -- excellent for exploring high spaces.",
+        name: "Large Cyclops",
+        imageName: "head-big-eye.png",
+        category: "Heads",
+        price: 1220.5,
+        discount: 0.2
+      },
+      {
+        id: 17,
+        description: "A spring base - great for reaching high places.",
+        name: "Spring Base",
+        imageName: "base-spring.png",
+        category: "Bases",
+        price: 1190.5,
+        discount: 0
+      },
+      {
+        id: 6,
+        description:
+          "An articulated arm with a claw -- great for reaching around corners or working in tight spaces.",
+        name: "Articulated Arm",
+        imageName: "arm-articulated-claw.png",
+        category: "Arms",
+        price: 275,
+        discount: 0
+      },
+      {
+        id: 2,
+        description:
+          "A friendly robot head with two eyes and a smile -- great for domestic use.",
+        name: "Friendly Bot",
+        imageName: "head-friendly.png",
+        category: "Heads",
+        price: 945.0,
+        discount: 0.2
+      },
+      {
+        id: 3,
+        description:
+          "A large three-eyed head with a shredder for a mouth -- great for crushing light medals or shredding documents.",
+        name: "Shredder",
+        imageName: "head-shredder.png",
+        category: "Heads",
+        price: 1275.5,
+        discount: 0
+      }
+    ];
   }
 
-  getImageUrl(product: IProduct) {
+  getImage(product: IProduct) {
     return '/assets/images/robot-parts/' + product.imageName;
   }
 
+  getFilteredProducts() {
+    return this.filter === '' ? this.products :
+      this.products.filter((product) => product.category === this.filter);
+  }
 }
